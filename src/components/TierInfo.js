b433005f81a442912a68999cc5c5ba2f2e3f8108
Abhay Maurya <abhaymaurya452002@gmail.com>
2023-03-05 03:56:00 +0000

    loyalty card

--- src/components/TierInfo.js
+++ src/components/TierInfo.js
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import {
   Box,
   Flex,
   Heading,
+  Grid, GridItem,
   Text,
 } from "@chakra-ui/react";
+import { getLoyalityTiers } from "../services/APIService";
+
 
 
 export default function TierInfo() {
+  const [loyaltyTiers, setLoyaltyTiers] = useState([])
+
+  useEffect(() => {
+    getLoyalityTiers().then(response => {
+      setLoyaltyTiers(response)
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  if (!loyaltyTiers || loyaltyTiers === []) {
+    return (
+      <Flex>
+        <Text>Loading</Text>
+      </Flex>
+    )
+  }
+
+
   return (
     <Box bg="white" py={{ base: "24", sm: "32" }}>
       <Box mx="auto" maxW="7xl" px={{ base: "6", lg: "8" }}>
@@ -36,79 +59,85 @@ export default function TierInfo() {
             quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.
           </Text>
         </Box>
-        <Flex
+        <Grid
+          templateColumns="repeat(5, 1fr)" // 3 columns in this example
           mx="auto"
           mt="16"
           maxW={{ base: "2xl", lg: "none" }}
           rounded={{ base: "none", lg: "3xl" }}
           borderWidth={{ base: "0", lg: "1px" }}
           borderColor="gray.200"
-          flexDirection={{ base: "column", lg: "row" }}
           overflow="hidden"
+          gap={2}
         >
-          <Box
-            mt={{ base: "-2", lg: "0" }}
-            p={{ base: "2", lg: "0" }}
-            flex={{ base: "none", lg: "none" }}
-            maxW={{ lg: "md" }}
-            flexGrow={{ lg: "0" }}
-          >
-            <Box
-              rounded="2xl"
-              bg="gray.50"
-              py="10"
-              textAlign="center"
-              borderWidth="1px"
-              borderStyle="inset"
-              borderColor="gray.900/5"
-              display={{ base: "none", lg: "flex" }}
-              flexDirection="column"
-              justifyContent="center"
-              // py={{ "lg": "16" }}
-            >
-              <Box mx="auto" maxW="xs" px="8">
-                <Text fontSize="base" fontWeight="semibold" color="gray.600">
-                  Pay once, own it forever
-                </Text>
-                <Flex
-                  alignItems="baseline"
+          {console.log("THIS IS ME", loyaltyTiers)}
+          {loyaltyTiers?.loyality_tiers?.map((loyaltyTier) => (
+            <GridItem colSpan={1} rowSpan={1} >
+              <Box
+                mt={{ base: "-2", lg: "0" }}
+                p={{ base: "2", lg: "0" }}
+                
+                
+              >
+                <Box
+                  rounded="2xl"
+                  bg="gray.50"
+                  py="10"
+                  textAlign="center"
+                  borderWidth="1px"
+                  // borderStyle="inset"
+                  borderColor="gray.900/5"
+                  display={{ base: "none", lg: "flex" }}
+                  flexDirection="column"
                   justifyContent="center"
-                  mt="6"
-                  mb="10"
+                  h="300px"
+                // py={{ "lg": "16" }}
                 >
-                  <Text
-                    fontSize="5xl"
-                    fontWeight="bold"
-                    tracking="tight"
-                    color="gray.900"
-                  >
-                    $349
-                  </Text>
-                  <Text
-                    fontSize="sm"
-                    fontWeight="semibold"
-                    lineHeight="6"
-                    color="gray.600"
-                    ml="2"
-                  >
-                    USD
-                  </Text>
-                </Flex>
-                <Box>
-                  <a
-                    href="#"
-                    className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Get access
-                  </a>
+                  <Box mx="auto" maxW="xs" px="8">
+                    
+                    <Flex
+                      alignItems="baseline"
+                      justifyContent="center"
+                      mt="6"
+                      mb="10"
+                    >
+                      <Text
+                        fontSize="3xl"
+                        fontWeight="bold"
+                        tracking="tight"
+                        color="gray.900"
+                      >
+                        {loyaltyTier.name}
+                      </Text>
+                      
+                    </Flex>
+                    <Box>
+                    
+                    <Text fontSize="base" fontWeight="semibold" color="gray.600">
+                     £{loyaltyTier.range_min} - {loyaltyTier.range_max}
+                    </Text>
+                    
+                    </Box>
+                    {/* <Text fontSize="xs" lineHeight="5" color="gray.600" mt="6">
+                      Invoices and receipts available for easy company reimbursement
+                    </Text> */}
+                  </Box>
                 </Box>
-                <Text fontSize="xs" lineHeight="5" color="gray.600" mt="6">
-                  Invoices and receipts available for easy company reimbursement
-                </Text>
               </Box>
-            </Box>
-          </Box>
-        </Flex>
+
+              {/* Content for the first two cells */}
+            </GridItem>
+          ))}
+
+
+
+
+        </Grid>
+
+
+
+
+
       </Box>
     </Box>
   );
